3d997e8a4dbe49718a038fb4c81590c3a4fcbb12
Ivan Kozlov <69389177+IYunet@users.noreply.github.com>
2023-04-12 23:26:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,10 +8,14 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    alpha = 0.02
+    alpfa = 0.02
     p1 = x_success / x_cnt
     p2 = y_success / y_cnt
-    standrart_error = ((p1*(1-p1))/x_cnt + (p2*(1-p2))/y_cnt)**0.5
-    z = abs((p1 - p2) / standrart_error)
-    critical_value = abs(norm.ppf(alpha/2))
-    return z > critical_value
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    z = (p1 - p2)/(p * (1-p) * ((1/x_cnt) + (1/y_cnt))) ** 0.5
+    p_val = norm.sf(z)
+    
+    if p_val < alpfa:
+      return True
+    else: 
+      return False
