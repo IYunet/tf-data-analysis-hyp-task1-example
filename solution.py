72fcb7c7a6a829f3c8e597f8957ffb8ca54015a5
Ivan Kozlov <69389177+IYunet@users.noreply.github.com>
2023-04-12 22:41:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 426527714 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.02
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    standrart_error = ((p1*(1-p1))/x_cnt + (p2*(1-p2))/y_cnt)**0.5
+    z = abs((p1 - p2) / standrart_error)
+    critical_value = abs(norm.ppf(alpha/2))
+    return z > critical_value
